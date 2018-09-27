de4ceb24500c9b3566b277bf43c08770f9dc2cfb
faridddd <faridrahbari3@gmail.com>
2018-09-27 16:40:55 +0330

    doing the necessary changes

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const Handlebars = require('handlebars')
 const multer = require('multer');
 const UserSchema = require("./models/User");
 const PostSchema = require("./models/Post");
+const routes = require("./routes/index");
 var upload = multer({
     dest: 'uploads/'
 })
@@ -45,12 +46,6 @@ mongoose.connection.on("error",(err)=>{
     console.log(err);
 })
 
-mongoose.Promise = global.Promise;
-
-
-const User = mongoose.model('users', UserSchema);
-
-const Post = mongoose.model('posts', PostSchema);
 // middlewares
 
 app.use(session({
@@ -128,155 +123,4 @@ app.engine('hbs', expHbs({
 }));
 app.use(express.static(__dirname + '/views/public'))
 
-
-// routes
-
-app.get('/', (req, res) => {
-    Post.find()
-        .then(data => {
-            res.render('main/index.hbs', {
-                data: data 
-            })
-        })
-})
-
-app.get('/signup', (req, res) => {
-    res.render('main/signup.hbs')
-})
-
-app.post('/signup', (req, res) => {
-    User.findOne({
-            email: req.body.email
-        })
-        .then(data => {
-            if (data == null) {
-                bcrypt.genSalt(16, (err, salt) => {
-                    if (err) {
-                        throw err;
-                    }
-                    bcrypt.hash(req.body.password, salt, (error, hash) => {
-                        if (error) {
-                            throw error
-                        }
-                        newUser = new User({
-                            email: req.body.email,
-                            password: hash,
-                            username: req.body.username
-                        })
-                        newUser.save()
-                            .then(user => {
-                                req.flash('Smsg', 'Account created successfully')
-                                res.redirect('/login')
-                            })
-                    })
-                })
-            } else {
-                req.flash('Fmsg', 'Account exists')
-                res.redirect('/signup')
-            }
-        })
-})
-
-app.post('/login', (req, res, next) => {
-    passport.authenticate('local', {
-        successRedirect: '/',
-        failureRedirect: '/login',
-        failureFlash: true,
-        successFlash: true
-    })(req, res, next)
-})
-app.get('/login', (req, res) => {
-    res.render('main/login.hbs')
-})
-
-app.post('/addnewpost', (req, res) => {
-    if (req.user) {
-
-        const newPost = new Post({
-            title: req.body.title,
-            image: req.body.image,
-            matn: req.body.matn
-        })
-        newPost.save()
-            .then(data => {
-                res.redirect('/')
-            })
-    } else {
-        req.flash('Fmsg', 'you\'re not autherizede !')
-        res.redirect('/login')
-    }
-})
-
-app.get('/addnewpost', (req, res) => {
-    res.render('main/addnewpost.hbs')
-})
-app.get('/:id', (req, res) => {
-    var id = req.params.id
-    Post.findById({
-            _id: id
-        })
-        .then(data => {
-            res.render('main/show.hbs', {
-                data: data
-            })
-        })
-
-})
-
-app.post('/posts/comment/:id', (req, res) => {
-    let id = req.params.id
-    // console.log(req.user.username);
-    if (req.user) {
-
-        Post.findById({
-                _id: req.params.id
-            })
-            .then(data => {
-                const newComment = {
-                    commentBody: req.body.cmbody,
-                    commentUser: req.user.username
-                }
-                data.comments.unshift(newComment)
-
-                data.save()
-                    .then(data => {
-                        res.redirect('/:'+id)
-                    })
-            })
-    } else {
-        req.flash('Fmsg', 'you are not logged in')
-        res.redirect('/login')
-    }
-
-})
-
-app.post('/search', (req, res) => {
-    Post.find({
-            title: req.body.title
-        })
-        .then(data => {
-            res.render('main/show.hbs', {
-                data: data
-            })
-        })
-})
-
-app.post('/delete', (req, res) => {
-    if (req.body.token == token) {
-        Post.findByIdAndRemove({
-                _id: req.body.id
-            })
-            .then(data => {
-                req.flash('Smsg', 'Post deleted')
-                res.redirect('/')
-            })
-    } else {
-        req.flash('Fmsg', 'authentication error')
-        res.redirect('/')
-    }
-})
-
-app.get('/robots.txt', (req, res) => {
-    res.type('text/plain');
-    res.send("User-agent: *\nDisallow: /");
-})
+app.use('/',routes);
